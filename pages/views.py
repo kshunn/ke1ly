ab5c9b74532c875d8908c86674b84d2f06c00631
kshunn <qkshunn@gmail.com>
2020-02-27 23:31:53 +0900

    team.html: color modification pages authentication added

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def about(request):
+    if not request.user.is_authenticated:
+        return render(request, 'login.html')
     return render(request, 'about.html')
 
 
 def main(request):
+    if not request.user.is_authenticated:
+        return render(request, 'login.html')
     return render(request, 'main.html')
 
 
 def team(request):
+    if not request.user.is_authenticated:
+        return render(request, 'login.html')
     return render(request, 'team.html')
