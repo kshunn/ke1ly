9a31b87e2c5856257741666b7236f2d9fc4f2af6
kshunn <qkshunn@gmail.com>
2020-02-27 20:30:15 +0900

    login/signup: added alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 
 def aboutus(request):
@@ -16,7 +17,8 @@ def login(request):
             auth.login(request, user)
             return redirect('main')
         else:
-            return render(request, 'login.html', {'error': 'Username or password is incorrect'})
+            messages.success(request, 'Username or password is incorrect')
+            return render(request, 'login.html')
     else:
         return render(request, 'login.html')
 
@@ -31,7 +33,9 @@ def signup(request):
         if request.POST["pw1"] == request.POST["pw2"]:
             user = User.objects.create_user(
                 username=request.POST["id"], password=request.POST["pw1"])
+            messages.success(request, 'Sign up successful')
             return render(request, 'login.html')
-        return render(request, 'signup.html', {'error': 'Confirm your password'})
+        messages.success(request, 'Confirm your password')
+        return render(request, 'signup.html')
     return render(request, 'signup.html')
 
