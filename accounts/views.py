0c5adc8c1d5587a48ca62d210d9eee66467f2877
kshunn <qkshunn@gmail.com>
2020-03-01 14:44:27 +0900

    alert for already existing username

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib import messages
-from django.views.decorators.csrf import ensure_csrf_cookie
 
 
 def aboutus(request):
     return render(request, 'aboutus.html')
 
 
-@ensure_csrf_cookie
 def login(request):
     if request.user.is_authenticated:
         return redirect('main')
@@ -35,10 +33,17 @@ def logout(request):
 def signup(request):
     if request.method == "POST":
         if request.POST["pw1"] == request.POST["pw2"]:
-            user = User.objects.create_user(
-                username=request.POST["id"], password=request.POST["pw1"])
-            messages.success(request, 'Sign up successful')
-            return render(request, 'login.html')
+            flag = True
+            for user in User.objects.all():
+                if user.username == request.POST["id"]:
+                    flag = False
+            if flag:
+                User.objects.create_user(username=request.POST["id"], password=request.POST["pw1"])
+                messages.success(request, 'Sign up successful')
+                return render(request, 'login.html')
+            else:
+                messages.error(request, 'Username already exists')
+                return render(request, 'signup.html')
         messages.error(request, 'Confirm your password')
         return render(request, 'signup.html')
     return render(request, 'signup.html')
