5ff49a84b4163caee8c0d5a1b5ddeca5fea1449d
kshunn <qkshunn@gmail.com>
2020-03-01 20:12:21 +0900

    font added

--- static/pages/main.js
+++ static/pages/main.js
@@ -1,13 +1,10 @@
-$(".menu-toggle-btn").click(function(){
-  $(this).toggleClass("fa-times");
-  $(".navigation-menu").toggleClass("active");
+$(".del").click(function(){
+  $(".middle").toggleClass("inactive");
+  $(".middle2").toggleClass("active");
 });
 
-
-$(".owl-carousel").owlCarousel({
-  items: 2,
-  margin: 20,
-  loop: true,
-  center: true,
-  dots: false
+$(".no").click(function(){
+  $(".middle").toggleClass("inactive");
+  $(".middle2").toggleClass("active");
 });
+
